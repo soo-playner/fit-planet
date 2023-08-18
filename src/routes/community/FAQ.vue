f3220581497d17043573d63fbc018dcdb343f3f3
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-08-18 15:30:36 +0900

    faq 탭 애니메이션 스크립트

--- src/routes/community/FAQ.vue
+++ src/routes/community/FAQ.vue
@@ -1,21 +1,46 @@
 <script setup>
-import { ref } from "vue";
+import { EffectCoverflow } from "swiper";
+import { ref, onMounted } from "vue";
 
-const active = ref("회원정보");
+//const active = ref("회원정보");
 const faqList = ["회원정보", "결제", "환불", "후기", "오류", "기타"];
 
-const changeActive = (faqItem) => {
-    active.value = faqItem;
-};
+// const changeActive = (faqItem) => {
+//     active.value = faqItem;
+// };
+
+onMounted(() => {
+    const faqLi = document.querySelectorAll('.faq-li');
+    const nav = document.querySelector('.nav-indicator');
+
+    function navFnc(el){
+        // 클릭 시 갖고 있던 active 삭제
+        faqLi.forEach((items) => {
+            items.classList.remove('active');
+        });
+        
+        // active 클래스를 가진 li 태그의 값 가져오도록
+        nav.style.width = `${el.clientWidth}px`;
+        nav.style.left = `${el.offsetLeft}px`;
+        nav.style.bottom = '0';
+
+        // active 추가
+        el.classList.add('active');
+    }
+
+    faqLi.forEach((items) => {
+        items.addEventListener('click', (e) => { navFnc(e.target) });
+        items.classList.contains('active') && navFnc(items);
+    });
+});
 </script>
 
 <template>
     <div class="FAQ_container main-layout">
         <div class="FAQ_container_inner mob-inner">
             <ul class="faq-tab-list">
-                <li v-for="faqItem in faqList" :key="faqItem" @click="changeActive(faqItem)" :class="{ active: active === faqItem }">
-                    <button>{{ faqItem }}</button>
-                </li>
+                <li class="faq-li active" v-for="faqItem in faqList" :key="faqItem">{{ faqItem }}</li> 
+                <span class="nav-indicator"></span>
             </ul>
         </div>
     </div>
