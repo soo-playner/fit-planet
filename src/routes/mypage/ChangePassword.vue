8c3f14c574710c4b28ee06a9ed5fcbc16d955024
yonghwan <coxldtpeo1@naver.com>
2023-08-22 17:07:17 +0900

    닉네임, 비밀번호 변경 페이지 유효성 검사기능 추가

--- src/routes/mypage/ChangePassword.vue
+++ src/routes/mypage/ChangePassword.vue
@@ -1,4 +1,6 @@
 <script setup>
+import useValidations from "@/composables/useValidations";
+const { form } = useValidations();
 </script>
 
 <template>
@@ -7,7 +9,7 @@
             <div class="ChangePassword-guide txt-box">
                 <p class="f-14-400">기존에 사용했던 비밀번호를 입력해주세요</p>
                 <div class="form-group">
-                    <input type="text" name="mb-nick" id="mb-nick" placeholder="위즈위즈">
+                    <input v-model="form.mb_password.value" type="text" name="mb-nick" id="mb-nick" placeholder="" />
                 </div>
             </div>
             <button class="changePassword-btn bott-btn f-16-700" @click="$router.push('/Change/Password/Confirm')">비밀번호 변경하기</button>
--- src/routes/mypage/ChangePassword_Confirm.vue
+++ src/routes/mypage/ChangePassword_Confirm.vue
@@ -1,5 +1,6 @@
 <script setup>
-
+import useValidations from "@/composables/useValidations";
+const { form, isFormValid, errorText, nextCondition } = useValidations();
 </script>
 
 <template>
@@ -8,17 +9,19 @@
             <div class="txt-box">
                 <p class="f-14-400">새 비밀번호를 입력해주세요</p>
                 <div class="form-group">
-                    <input type="password" name="mb-password" id="mb-password" placeholder="새 비밀번호 입력">
+                    <input v-model="form.mb_password.value" type="password" name="mb-password" id="mb-password" placeholder="새 비밀번호 입력" />
                 </div>
+                <p v-show="form.mb_password.value && !isFormValid.mb_password.value">{{ errorText.mb_password }}</p>
                 <div class="form-group">
-                    <input type="password" name="mb-password" id="mb-password" placeholder="새 비밀번호 확인">
+                    <input v-model="form.mb_password_cfm.value" type="password" name="mb-password" id="mb-password" placeholder="새 비밀번호 확인" />
                 </div>
+                <p v-show="form.mb_password_cfm.value && !isFormValid.mb_password_cfm.value">{{ errorText.mb_password_cfm }}</p>
                 <div class="password-guide f-12-400">
                     <p>영문, 숫자, 특수문자 조합 8자리 이상</p>
                     <p>특수문자는 ~’!@#$%^&*()-만 사용 가능합니다.</p>
                 </div>
             </div>
-            <button class="changePassword-btn next-step-btn f-16-700" @click="$router.push('/Change/Password/Confirm')">비밀번호 변경하기</button>
+            <button class="changePassword-btn next-step-btn f-16-700" @click="$router.push('/Change/Password/Confirm')" :disabled="!nextCondition()">비밀번호 변경하기</button>
         </div>
     </div>
 </template>
