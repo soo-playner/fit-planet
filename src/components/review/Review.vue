1ff546c267d67c52770fd0b2283a503c1e245f83
Jaewon <woniyoon@gmail.com>
2023-08-22 15:50:09 +0900

    플레이스 후기 내 리뷰 영역 컴포넌트화

---
+++ src/components/review/Review.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { defineProps, ref } from "vue";
+
+const props = defineProps({
+    review: Object
+});
+
+const {comment, id, date, likeCnt, rating, reply, replyDate, writer} = props.review;
+
+// 액션버튼
+const isOptionMenuOpen = ref(false);
+
+const openOptionMenu = () => {
+    isOptionMenuOpen.value = !isOptionMenuOpen.value;
+}
+
+</script>
+
+<template>
+    <div class="total-review-form" >
+        <div class="review-writer">
+            <div class="left-row">
+                <div class="user-profile"></div>
+                <div class="writer-info">
+                    <p class="f-14-700">{{writer}}<span class="f-12-400">{{date}}</span></p>
+                    <img src="@/assets/image/star_box.png" alt="">
+                </div>
+            </div>
+            <div class="right-row">
+                <div class="like-box"><img src="@/assets/image/like.png" alt="좋아요"><span class="like-count f-14-700">{{likeCnt}}</span></div>
+                <div class="menu-dot">
+                    <span @click="openOptionMenu"></span>
+                    <ul class="review-sorting-list" v-show="isOptionMenuOpen">
+                        <li><router-link to="/">공유하기</router-link></li>
+                        <li><router-link to="/">신고하기</router-link></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="review-content">
+            <div class="member-review">
+                <div class="member-review-txt f-14-400">{{comment}}</div>
+                <div class="member-review-img"></div>
+            </div>
+            <div class="place-answer">
+                <p class="f-14-700">플레이스 답변<span class="f-12-400">{{replyDate}}</span></p>
+                <div class="placep-answer-txt f-14-400">{{reply}}</div>
+            </div>  
+        </div>
+    </div>
+</template>
--- src/routes/place/PlaceReview.vue
+++ src/routes/place/PlaceReview.vue
@@ -1,4 +1,10 @@
 <script setup>
+import Review from '@/components/review/Review.vue';
+import {useStore} from 'vuex'
+
+const store = useStore();
+const reviewsByPlaces = store.state.review.reviewsByPlaces;
+
 </script>
 
 <template>
@@ -16,68 +22,7 @@
                     <li><p class="f-12-400">추천순</p><span class="arrow"></span></li>
                 </ul>
             </div>
-            <div class="total-review-form">
-                <div class="review-writer">
-                    <div class="left-row">
-                        <div class="user-profile"></div>
-                        <div class="writer-info">
-                            <p class="f-14-700">위즈위즈<span class="f-12-400">3개월 전</span></p>
-                            <img src="@/assets/image/star_box.png" alt="">
-                        </div>
-                    </div>
-                    <div class="right-row">
-                        <div class="like-box"><img src="@/assets/image/like.png" alt="좋아요"><span class="like-count f-14-700">6</span></div>
-                        <div class="menu-dot">
-                            <span></span>
-                            <ul class="review-sorting-list">
-                                <li><router-link to="/">공유하기</router-link></li>
-                                <li><router-link to="/">신고하기</router-link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="review-content">
-                    <div class="member-review">
-                        <div class="member-review-txt f-14-400">운동기구도 다양하고 운동복, 수건도 쓸 수 있어서 너무 만족스러웠습니다. 집 근처라 꾸준히 이용하게 될 것 같아요.</div>
-                        <div class="member-review-img"></div>
-                    </div>
-                    <div class="place-answer">
-                        <p class="f-14-700">플레이스 답변<span class="f-12-400">3개월 전</span></p>
-                        <div class="placep-answer-txt f-14-400">위즈위즈 회원님 후기 감사합니다! 앞으로도 저희 플레이스 많이 이용해주세요!</div>
-                    </div>  
-                </div>
-            </div>
-            <div class="total-review-form">
-                <div class="review-writer">
-                    <div class="left-row">
-                        <div class="user-profile"></div>
-                        <div class="writer-info">
-                            <p class="f-14-700">위즈위즈<span class="f-12-400">3개월 전</span></p>
-                            <img src="@/assets/image/star_box.png" alt="">
-                        </div>
-                    </div>
-                    <div class="right-row">
-                        <div class="like-box"><img src="@/assets/image/like.png" alt="좋아요"><span class="like-count f-14-700">6</span></div>
-                        <div class="menu-dot">
-                            <span></span>
-                            <ul class="review-sorting-list">
-                                <li><router-link to="/">공유하기</router-link></li>
-                                <li><router-link to="/">신고하기</router-link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="review-content">
-                    <div class="member-review">
-                        <div class="member-review-txt f-14-400">운동기구도 다양하고 운동복, 수건도 쓸 수 있어서 너무 만족스러웠습니다. 집 근처라 꾸준히 이용하게 될 것 같아요.</div>
-                        <div class="member-review-img"></div>
-                    </div>
-                    <div class="place-answer">
-                        <p class="f-14-700">플레이스 답변<span class="f-12-400">3개월 전</span></p>
-                        <div class="placep-answer-txt f-14-400">위즈위즈 회원님 후기 감사합니다! 앞으로도 저희 플레이스 많이 이용해주세요!</div>
-                    </div>  
-                </div>
-            </div>
+            <Review v-for="review in reviewsByPlaces" :key="review.id" :review="review" />
         </div>
     </div>
 </template> 
