3e04eed790cee2f53946f9e27eee02927ef3d2a4
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-08-24 16:58:15 +0900

    디테일 수정

--- src/routes/mypage/ChangePassword_Confirm.vue
+++ src/routes/mypage/ChangePassword_Confirm.vue
@@ -19,7 +19,7 @@ const { form, isFormValid, errorText, nextCondition } = useValidations();
                         </span>
                     </div>
                 </div>
-                <p v-show="form.mb_password.value && !isFormValid.mb_password.value">{{ errorText.mb_password }}</p>
+                <p class="vail f-12-400" v-show="form.mb_password.value && !isFormValid.mb_password.value">{{ errorText.mb_password }}</p>
                 <div class="form-group">
                     <input v-model="form.mb_password_cfm.value" type="password" name="mb-password" id="mb-password" placeholder="새 비밀번호 확인" />
                     <div class="etc">
@@ -31,7 +31,7 @@ const { form, isFormValid, errorText, nextCondition } = useValidations();
                         </span>
                     </div>
                 </div>
-                <p v-show="form.mb_password_cfm.value && !isFormValid.mb_password_cfm.value">{{ errorText.mb_password_cfm }}</p>
+                <p class="vail f-12-400" v-show="form.mb_password_cfm.value && !isFormValid.mb_password_cfm.value">{{ errorText.mb_password_cfm }}</p>
                 <div class="password-guide f-12-400">
                     <p>영문, 숫자, 특수문자 조합 8자리 이상</p>
                     <p>특수문자는 ~’!@#$%^&*()-만 사용 가능합니다.</p>
