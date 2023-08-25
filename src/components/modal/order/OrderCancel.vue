0460ada196f64fdf189a67bbb8182c95d60f20d3
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-08-25 16:25:31 +0900

    결제 취소, 결제 완료 후 일정 등록 모달창

---
+++ src/components/modal/order/OrderCancel.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+    <div class="order-cancel-modal member_alert">
+        <div class="overlay"></div>
+        <div class="order-cancel-modal-inner mob-inner member_alert_inner">
+            <div class="txt-box">
+                <div class="f-18-700">
+                    <p>잠시만요!</p>
+                    <p>결제를 그만두고 나가시겠어요?</p>
+                </div>
+                <p class="f-14-400">
+                    <span>이 페이지를 벗어날 경우,</span>
+                    <span>지금까지 작성하신 내용이 모두 사라져요.</span>
+                </p>
+            </div>
+            <div class="bott-btn">
+                <button class="f-16-700">나가기</button>
+                <button class="f-16-700">머무르기</button>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/modal/order/ScheduleRegister.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+    <div class="schedule-register-modal member_alert">
+        <div class="overlay"></div>
+        <div class="schedule-register-modal-inner mob-inner member_alert_inner">
+            <div class="txt-box">
+                <div class="f-18-700">
+                    <p>트레이너와의</p>
+                    <p>운동 일정이 정해졌나요?</p>
+                </div>
+                <p class="f-14-400">
+                    <span>운동 시작일과 요일을 등록하면</span>
+                    <span>수강권을 쉽게 관리할 수 있어요</span>
+                </p>
+            </div>
+            <div class="bott-btn">
+                <button class="f-16-700">나중에 하기</button>
+                <button class="f-16-700">일정 등록하기</button>
+            </div>
+        </div>
+    </div>
+</template>
