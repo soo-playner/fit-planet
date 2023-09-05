2ad8d4f0e082e8bceb9fc839f0e2eb5cd97f3b75
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-09-05 14:06:48 +0900

    디테일 수정 / 회원정보X 모달창

---
+++ src/components/modal/password/NotFindMember.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+    <div class="member_alert notFind-member">
+        <div class="overlay" @click="ResetPWFnc"></div>
+        <div class="member_alert_inner mob-inner notFind-member-inner">
+            <div class="txt-box f-18-700">
+                가입된 회원 정보가 없습니다.
+            </div>
+            <router-link to="/login" class="btn1">확인</router-link>
+        </div>
+    </div>
+</template>
